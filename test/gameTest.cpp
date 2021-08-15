9b4bf22bea62eb42809af7f39f0e9d2b6116f06f
Tudor Oancea <oancea.tudor@icloud.com>
2021-08-15 11:25:20 +0200

    Modified CMakeLists to create a static lib to be linked to the tests

---
+++ test/gameTest.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Tudor Oancea on 14/08/2021.
+// Copyright (c) 2021 Tudor Oancea & Mattéo Berthet. All rights reserved.
+// Licensed under the MIT license (see https://github.com/tudoroancea/quetzalcoatl/blob/develop/LICENSE)
+//
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma ide diagnostic ignored "cert-err58-cpp"
+
+#include "Game.h"
+
+#include <gtest/gtest.h>
+
+TEST(GameTest, regenerateApple) {
+    for (int i(0); i < 10000; ++i) {
+        ASSERT_NO_THROW(Game game);
+        // Game testGame;
+        // testGame
+    }
+}
+#pragma clang diagnostic pop
